e54963480b6efd558e09f004696b2878a87beff4
AdamCodeAccount <akuto1343@gmail.com>
2026-02-20 15:47:46 -0500

    Set up initial project structure and dependencies with Create React App

---
+++ src/config/images.js
@@ -0,0 +1,28 @@
+/**
+ * Configuration des images de la galerie
+ * Ajoutez vos images dans public/img/lieu/ et public/img/couple/
+ * Formats acceptés: .jpg, .jpeg, .png, .webp
+ */
+
+// Images du lieu (lieu de la cérémonie)
+export const lieuImages = [
+  'lieu-1.jpg',
+  'lieu-2.jpg',
+  'lieu-3.jpg',
+  'lieu-4.jpg',
+  'lieu-5.jpg',
+];
+
+// Images du couple
+export const coupleImages = [
+  'couple-1.jpg',
+  'couple-2.jpg',
+  'couple-3.jpg',
+  'couple-4.jpg',
+  'couple-5.jpg',
+  'couple-6.jpg',
+];
+
+// Chemins complets pour les composants
+export const getLieuImagePath = (filename) => `/img/lieu/${filename}`;
+export const getCoupleImagePath = (filename) => `/img/couple/${filename}`;
